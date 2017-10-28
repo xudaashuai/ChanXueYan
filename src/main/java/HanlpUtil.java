1911e81d417e2dc144bd79778b00f0c9d85c573f
xudashuai <xudashuai0819@outlook.com>
2017-10-28 13:20:14 +0800

    添加多规则处理

--- src/main/java/HanlpUtil.java
+++ src/main/java/HanlpUtil.java
@@ -25,7 +25,12 @@ public class HanlpUtil {
 
     }
 
-    public void parseFile(SearchList list) throws IOException {
+    public void parseFile(SearchList rule) throws IOException {
+        ArrayList<SearchList> rules=new ArrayList<SearchList>();
+        rules.add(rule);
+        parseFileWithRules(rules);
+    }
+    public void parseFileWithRules(ArrayList<SearchList> list) throws IOException {
         String pathname = "comment.txt";
         File filename = new File(pathname); // 要读取以上路径的input。txt文件
         InputStreamReader reader = null; // 建立一个输入流对象reader
@@ -43,13 +48,24 @@ public class HanlpUtil {
         while (line != null) {
             lineno += 1;
             if(lineno%100==0)
-            System.out.println("测试了"+lineno +"组测试用例，"+okno+"组测试用例匹配该规则");
+                System.out.println("测试了"+lineno +"组测试用例，"+okno+"组测试用例匹配该规则");
             line = br.readLine(); // 一次读入一行数据
+            if (line==null){
+                break;
+            }
             CoNLLSentence sentence = parseSentence(line);
-            ArrayList<Integer> words = list.search(sentence);
-            if (words.size()!=0)okno+=1;
-            for (int index : words) {
-                System.out.printf("%d %s %s %s\n", lineno, sentence.getWordArray()[index].NAME, sentence.getWordArray()[index + 1].NAME, sentence.getWordArray()[index + 2].NAME);
+            for (SearchList rule:list) {
+                ArrayList<Integer> words = rule.search(sentence);
+                if (words.size()!=0) {
+                    okno += 1;
+                    for (int index : words) {
+                        System.out.print(lineno);
+                        for (int i = 0; i < rule.nodes.size(); i++) {
+                            System.out.print(" "+sentence.getWordArray()[index+i].NAME+" ");
+                        }
+                        System.out.println();
+                    }
+                }
             }
         }
         System.out.println("一共有 10000 组测试用例，"+okno+"组测试用例匹配该规则");
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         HanlpUtil hanlpUtil = new HanlpUtil();
-        hanlpUtil.parseSentence("这里写你想要解析的句子");
+        hanlpUtil.parseSentence("这个火锅好吃。");
         // 然后 用 dependencyView 打开 1.txt 就可以看见效果了
     }
 }
